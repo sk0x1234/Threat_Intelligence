f26e925d2ca65e0a75e85d74b06396996d6788d5
Srinath koilakonda <srinath.koilakonda@gmail.com>
2020-03-31 15:13:24 -0400

    Create Malwar-check.py

---
+++ Malwar-check.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import hashlib
+
+if sys.argc <1 :
+  print("Usage Malware-check  <FILEPATH>")
+  sys.exit(1)
+
+file = sys.argv[1] 
+B_SIZE = 65536 # The size of each read from the file
+
+f_hash = hashlib.sha256() # Create the hash object, can use something other than `.sha256()` if you wish
+with open(file, 'rb') as f: # Open the file to read it's bytes
+    fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+    while len(fb) > 0: # While there is still data being read from the file
+        f_hash.update(fb) # Update the hash
+        fb = f.read(BLOCK_SIZE) # Read the next block from the file
+
+print (f_hash.hexdigest()) # Get the hexadecimal digest of the hash
